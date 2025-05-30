764ff7332c4f76aa536d95f67cfd319aa39f6a7e
Henry Barreto <me@henrybarreto.dev>
2025-05-30 19:03:24 -0300

    feat: add request body limit

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -7,35 +7,37 @@ use tokio::net::TcpListener;
 use axum::{
     Router,
     extract::Extension,
-    http::{self, HeaderName},
+    http::{self, HeaderName, HeaderValue},
     routing::get,
     serve,
 };
 
 use tower::ServiceBuilder;
 use tower_http::{
-    self, compression::CompressionLayer, cors::CorsLayer, propagate_header::PropagateHeaderLayer,
-    trace::TraceLayer,
+    self, compression::CompressionLayer, cors::CorsLayer, limit::RequestBodyLimitLayer,
+    propagate_header::PropagateHeaderLayer, trace::TraceLayer,
 };
+
+const REQUEST_BODY_LIMIT: usize = 16;
 
 pub async fn router<S: Sync + Send + 'static>(state: S) -> Router {
     let state = Arc::new(state);
 
-    // TODO: Receive `origins` as a router configuration.
-    let origins = ["https://televiu.fly.dev".parse().unwrap()];
-
     let service = ServiceBuilder::new()
         .layer(TraceLayer::new_for_http())
         .layer(CompressionLayer::new())
+        .layer(RequestBodyLimitLayer::new(REQUEST_BODY_LIMIT))
         .layer(PropagateHeaderLayer::new(HeaderName::from_static(
             "x-request-id",
         )))
         .layer(
+            // TODO: Receive `origins` as a router configuration.
             CorsLayer::new()
-                .allow_origin(origins)
+                .allow_origin([HeaderValue::from_str("https://televiu.fly.dev").unwrap()])
                 .allow_methods([http::Method::GET]),
         );
 
+    // TODO: Remove magic routes.
     let router = Router::new()
         .route("/ws/controller", get(handlers::controller))
         .route("/ws/player", get(handlers::player))
@@ -51,11 +53,9 @@ pub struct Config {
 }
 
 pub async fn listen(router: Router, config: Config) -> Result<(), Error> {
-    let server_address = format!("{}:{}", config.host, config.port);
+    let addr = format!("{}:{}", config.host, config.port);
 
-    // let origins = ["https://televiu.fly.dev".parse().unwrap()];
-
-    let listener = TcpListener::bind(server_address).await?;
+    let listener = TcpListener::bind(addr).await?;
 
     return serve(listener, router).await;
 }
